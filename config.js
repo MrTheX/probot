04786be40e2a548badd3c63e99dda18f6a478d3d
X <91824551+MrTheX@users.noreply.github.com>
2021-10-06 13:05:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ module.exports = {
   prefix: "/", 
   devs: ["279557901922729984"], ///تعديل اساسي ايدي حسابك
 };
-exports.Token = "ODkzNDU5MzA3MDQyMDU0MTc1.YVbw0g.rfDOeMyHm3GP5SVAnC2U7mqmIqQ";
+exports.Token = "ODkzNDU5MzA3MDQyMDU0MTc1.YVbw0g.mCdWChlrNRuUeKFXM3BGHug5mss";
